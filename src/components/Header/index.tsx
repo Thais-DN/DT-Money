27c9cd917087c9468b50a7ce9880c14c1dd79f0a
Thais-DN <142531680+Thais-DN@users.noreply.github.com>
2024-02-27 06:41:29 -0300

    Adicionando component search e itens

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,29 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logo from "../../assets/Logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logo} alt="" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>
+                            Nova transação
+                        </NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <Dialog.Portal>
+                        <Dialog.Overlay>
+                            <Dialog.Content>
+                                <Dialog.Title>Nova Transação</Dialog.Title>
+                                <Dialog.Close />
+                            </Dialog.Content>
+                        </Dialog.Overlay>
+                    </Dialog.Portal>
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     );
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,45 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import {
+    PriceHighLitgh,
+    TransactionsContainer,
+    TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <SearchForm />
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLitgh variant="income">
+                                    R$ 12.000.00
+                                </PriceHighLitgh>
+                            </td>
+                            <td>Vendas</td>
+                            <td>25/02/2024</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHighLitgh variant="outcome">
+                                    - R$ 59,00
+                                </PriceHighLitgh>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>26/02/2024</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     );
 }
