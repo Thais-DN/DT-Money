d9b21b87d118f7606c1874a95c779cd70f3d9ce0
Thais-DN <142531680+Thais-DN@users.noreply.github.com>
2024-02-29 08:15:03 -0300

    "consumindo" api usando o useContext

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
@@ -6,8 +7,10 @@ import {
     TransactionsContainer,
     TransactionsTable,
 } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionContext";
 
 export function Transactions() {
+    const { transactions } = useContext(TransactionsContext);
     return (
         <div>
             <Header />
@@ -17,26 +20,24 @@ export function Transactions() {
                 <SearchForm />
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighLitgh variant="income">
-                                    R$ 12.000.00
-                                </PriceHighLitgh>
-                            </td>
-                            <td>Vendas</td>
-                            <td>25/02/2024</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Hamburguer</td>
-                            <td>
-                                <PriceHighLitgh variant="outcome">
-                                    - R$ 59,00
-                                </PriceHighLitgh>
-                            </td>
-                            <td>Alimentação</td>
-                            <td>26/02/2024</td>
-                        </tr>
+                        {transactions.map((transaction) => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">
+                                        {transaction.description}
+                                    </td>
+                                    <td>
+                                        <PriceHighLitgh
+                                            variant={transaction.type}
+                                        >
+                                            {transaction.price}
+                                        </PriceHighLitgh>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            );
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
